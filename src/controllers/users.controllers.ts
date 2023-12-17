fd94fdcfab105e92b3bcdec4ae7fe12e52b38786
Salman Irfan <107026575+Salman-Irfan@users.noreply.github.com>
2023-12-17 10:43:47 +0500

    put request, and totally dynamic request with parameters

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,23 +1,60 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Controller, Get, Param, Post, Put, Req } from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('/users')
 export class UsersController {
   // methods for request handlers
   @Get('/profile')
-  getProfile(@Req() req: Request ) {
-    console.log(req.body)
+  getProfile(@Req() req: Request) {
+    console.log(req.body);
     return {
-      message: `json response from nest js`
-    }
+      message: `json response from nest js`,
+    };
   }
   // post request
   @Post('/profile')
-  postProfile(@Req() req: Request){
-    console.log(req.body)
+  postProfile(@Req() req: Request) {
+    console.log(req.body);
     return {
-      message: 'json response from nest js post request'
-    }
+      message: 'json response from nest js post request',
+    };
   }
-  // make dynamic put request in nest js
+  // Dynamic PUT request
+  @Put('/profile/:id/comments/:username/replies/:replyId')
+  putProfile(
+    @Req() req: Request,
+    @Param('id') id: string,
+    @Param('username') username: string,
+    @Param('replyId') replyId: string,
+  ) {
+    console.log(req.body);
+    console.log(`Received ID: ${id}`);
+    console.log(`Received Username: ${username}`);
+    console.log(`Received Reply ID: ${replyId}`);
+
+    // Perform your logic here
+
+    return {
+      message: `JSON response from NestJS dynamic PUT request`,
+    };
+  }
+  // totally dynamic parameters
+  @Put('/dynamic-profile/:id/*') // Use wildcard parameter to capture the rest of the route
+  dynamicPutProfile(
+    @Req() req: Request,
+    @Param('id') id: string,
+    @Param() params: Record<string, string>,
+  ) {
+    console.log(req.body);
+    console.log(`Received ID: ${id}`);
+
+    // params will contain all dynamic parameters after 'id'
+    console.log('Dynamic Parameters:', params);
+
+    // Perform your logic here
+
+    return {
+      message: `JSON response from NestJS dynamic PUT request`,
+    };
+  }
 }
